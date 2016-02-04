2868060fe504b25d2bc0d0910aa1c86b9c70a996
Gonzalo <gonzalocasal@gmail.com>
2016-02-04 19:32:46 -0300

    compress

--- model/contacto/captcha.php
+++ model/contacto/captcha.php
@@ -1,28 +1,20 @@
 <?php
-	session_start();
-	if (!isset($_SESSION["again"]) and (!isset($_POST["email"]))){
-		header("Location:".$_SERVER["DOCUMENT_ROOT"]."");
-	}
-	if (isset($_POST["email"])){
-		$_SESSION["nombre"] = $_POST["nombre"]; 
-		$_SESSION["email"] = $_POST["email"]; 
-		$_SESSION["mensaje"] = $_POST["mensaje"];
-	}
-	$activa=3;
-	$titulo='Contacto | Colgado';
-	$programacion='';
-	$descripcion='Colgado.TV es el mejor sitio web para ver TV y escuchar radio por internet en vivo las 24hs online, gratis y sin l&iacute;mites.';
-	$luces="no";
-	$ad="no";
-        $sub="'boton-submit'";
-	$cards[1]=' <script src="https://www.google.com/recaptcha/api.js?hl=es"></script>
-           		<form action="../controller/mail.php" id="form-captcha" method="post" >
-			  		<div class="g-recaptcha" data-sitekey="6LdsSQoTAAAAAGmhRnttiooSCafXzfM0YPoIdyNp"></div>
-      		  		<br/>
-      		  		<div id="enviar">
-					<input type="submit" value="ENVIAR" id="boton-submit"/><br>	
-					<a href="javascript:{}" class="boton" onclick="document.getElementById('.$sub.').click(); return false;">ENVIAR</a>
-					</div>
-		   		</form>';
-	include_once'../controller/prepare.php';
+session_start();
+if (!isset($_SESSION["again"]) and (!isset($_POST["email"]))){
+	header("Location:".$_SERVER["DOCUMENT_ROOT"]."");
+}
+if (isset($_POST["email"])){
+	$_SESSION["nombre"] = $_POST["nombre"]; 
+	$_SESSION["email"] = $_POST["email"]; 
+	$_SESSION["mensaje"] = $_POST["mensaje"];
+}
+$activa=3;
+$titulo='Contacto | Colgado';
+$programacion='';
+$descripcion='Colgado.TV es el mejor sitio web para ver TV y escuchar radio por internet en vivo las 24hs online, gratis y sin l&iacute;mites.';
+$luces="no";
+$ad="no";
+    $sub="'boton-submit'";
+$cards[1]=' <script src="https://www.google.com/recaptcha/api.js?hl=es"></script><form action="../controller/mail.php" id="form-captcha" method="post" ><div class="g-recaptcha" data-sitekey="6LdsSQoTAAAAAGmhRnttiooSCafXzfM0YPoIdyNp"></div><br/><div id="enviar"><input type="submit" value="ENVIAR" id="boton-submit"/><br><a href="javascript:{}" class="boton" onclick="document.getElementById('.$sub.').click(); return false;">ENVIAR</a></div></form>';
+include_once'../controller/prepare.php';
 ?>
--- model/tv/index.php
+++ model/tv/index.php
@@ -7,111 +7,15 @@ $luces='no';
 $ad='no';
 $programacion='';
 $descripcion='Colgado del Cable es el mejor sitio web para ver TV y escuchar radio por internet en vivo las 24hs online, gratis y sin l&iacute;mites.';
-$cards[1]='	<a href="/tv/telefe/" class="spritetv sprite-telefe"></a>&nbsp;
-	    	<a href="/tv/eltrece/" class="spritetv sprite-eltrece"></a>&nbsp;
-	    	<a href="/tv/america/" class="spritetv sprite-america"></a>
-	    	<br>
-	    	<a href="/tv/tvpublica/" class="spritetv sprite-tvp"></a>
-	    	<a href="/tv/canal9/" class="spritetv sprite-canal9"></a>';
-$cards[2]='	<a href="/tv/tn/" class="spritetv sprite-tn"></a>&nbsp;
-			<a href="/tv/c5n/" class="spritetv sprite-c5n"></a>&nbsp;
-			<a href="/tv/america24/" class="spritetv sprite-a24"></a>
-			<br>
-			<a href="/tv/telesur/" class="spritetv sprite-telesur"></a>
-			<a href="/tv/rt/" class="spritetv sprite-rt"></a>
-			<br>
-			<a href="/tv/cn23/" class="spritetv sprite-cn23"></a>
-			<a href="/tv/cronica/" class="spritetv sprite-cronicatv"></a>
-			<a href="/tv/canal26/" class="spritetv sprite-canal26"></a>';
-
-$cards[3]='	<a href="/tv/tycsports/" class="spritetv sprite-tycsports"></a>&nbsp;
-			<a href="/tv/dxtv/" class="spritetv sprite-dxtv"></a>
-			<a href="/tv/americasports/" class="spritetv sprite-americasports2"></a>
-			<a href="/tv/garage/" class="spritetv sprite-garage "></a>
-			<a href="/tv/directvsports/" class="spritetv sprite-directvsports  DirecTV Sports"></a>
-			<br>
-			<a href="/tv/espn/" class="spritetv sprite-espntv   ESPN"></a>
-			<a href="/tv/espn+/" class="spritetv sprite-espnplus   ESPN+"></a>
-			<br>
-			<a href="/tv/foxsports/" class="spritetv sprite-foxsports   Fox Sports"></a>
-			<a href="/tv/foxsports2/" class="spritetv sprite-foxsports2  Fox Sports 2"></a>';
-$cards[4]='	<a href="/tv/tec/" class="spritetv sprite-tec  Tecn&oacute;polis TV"></a>
-			<a href="/tv/encuentro/" class="spritetv sprite-encuentro  Encuentro"></a>
-			<br>
-			<a href="/tv/history/" class="spritetv sprite-history   History"></a>
-			<a href="/tv/discovery/" class="spritetv sprite-discovery   Discovery"></a>
-			<a href="/tv/natgeo/" class="spritetv sprite-natgeo  Nat Geo"></a>
-			<br>
-			<a href="/tv/id/" class="spritetv sprite-id   Investigation Discovery"></a>
-			<a href="/tv/trutv/" class="spritetv sprite-trutv   Trutv"></a>
-			<br>
-			<a href="/tv/tlc/" class="spritetv sprite-tlc   TLC"></a>
-			<a href="/tv/hyh/" class="spritetv sprite-hyh   h&#38;h"></a>
-			<a href="/tv/animalplanet/" class="spritetv sprite-animalplanet   Animal Planet"></a>';
-$cards[5]='	<a href="/tv/magazine/" class="spritetv sprite-magazine   Magazine"></a>&nbsp;&nbsp;
-			<a href="/tv/360/" class="spritetv sprite-360tv  360TV"></a>&nbsp;&nbsp;
-			<a href="/tv/construirtv/" class="spritetv sprite-construirtv   Construir TV"></a>
-			<a href="/tv/tvuniversidad/" class="spritetv sprite-tvuniversidad   TV Universidad"></a>
-			<br>
-			<a href="/tv/acuamayor/" class="spritetv sprite-acuamayor   Acua Mayor"></a>
-			<a href="/tv/qm/" class="spritetv sprite-qm   QM La Plata"></a>&nbsp;
-			<a href="/tv/argentinisima/" class="spritetv sprite-argentinisima   Argentin&iacute;sima"></a>&nbsp;
-			<a href="/tv/rural/" class="spritetv sprite-rural   Canal Rural"></a>
-			<a href="/tv/acuafederal/" class="spritetv sprite-acuafederal   Acua Federal"></a>';
-$cards[6]='	<a href="/tv/mtv/" class="spritetv sprite-mtv  MTV"></a>
-			<a href="/tv/vh1/" class="spritetv sprite-vh1  VH1"></a>
-			<a href="/tv/vevo/" class="spritetv sprite-vevo  VEVO"></a>
-			<a href="/tv/onetv/" class="spritetv sprite-onetv  One Tv"></a>
-			<a href="/tv/rocknpop/" class="spritetv sprite-rocknpoptv  Rock & Pop Se Ve"></a>
-			';
-$cards[7]='	<a href="/tv/pakapaka/" class="spritetv sprite-pakapaka  Paka Paka"></a>
-			<a href="/tv/tateti/" class="spritetv sprite-tateti  tateti"></a>
-			<a href="/tv/cartoon/" class="spritetv sprite-cartoon  Cartoon Network"></a>
-			<br>
-			<a href="/tv/nick/" class="spritetv sprite-nick  Nick"></a>
-			<a href="/tv/discoverykids/" class="spritetv sprite-discoverykids  Discovery Kids"></a>
-			<a href="/tv/disney/" class="spritetv sprite-disneychannel  Disney Channel"></a>
-			<a href="/tv/disneyjunior/" class="spritetv sprite-disneyjunior  Disney Junior"></a>
-			<a href="/tv/disneyxd/" class="spritetv sprite-disneyxd  Disney XD"></a>';
-$cards[8]=' <a href="/tv/fox/" class="spritetv sprite-fox  FOX"></a>
-			<a href="/tv/warner/" class="spritetv sprite-warner  Warner"></a>
-			<a href="/tv/fx/" class="spritetv sprite-fx  FX"></a>
-			<br>
-			<a href="/tv/ftv/" class="spritetv sprite-ftv  FTV"></a>
-			<a href="/tv/foxlife/" class="spritetv sprite-foxlife  Fox Life"></a>
-			<a href="/tv/e!/" class="spritetv sprite-e!  E!"></a>
-			<br>
-			<a href="/tv/universal/" class="spritetv sprite-universal  Universal"></a>
-			<a href="/tv/axn/" class="spritetv sprite-axn  AXN"></a>
-			<a href="/tv/sony/" class="spritetv sprite-sony  Sony"></a>
-			<br>
-			<a href="/tv/tbs/" class="spritetv sprite-tbs  TBS Veryfunny"></a>
-			<a href="/tv/comedycentral/" class="spritetv sprite-comedycentral  Comedy Central"></a>
-			<a href="/tv/syfy/" class="spritetv sprite-syfy  Syfy"></a>
-			<br>
-			<a href="/tv/gourmet/" class="spritetv sprite-gourmet  El Gourmet"></a>
-			<a href="/tv/ae/" class="spritetv sprite-ae  A&#38;E"></a>
-			<a href="/tv/infinito/" class="spritetv sprite-infinito  Infinito"></a>
-			<br>
-			<a href="/tv/glitz/" class="spritetv sprite-glitz  Glitz"></a>
-			<a href="/tv/tlnovelas/" class="spritetv sprite-tlnovelas  Tlnovelas"></a>
-			<a href="/tv/casaclub/" class="spritetv sprite-casaclub  Casa Club"></a>';
-$cards[9]=' <a href="/tv/tnt/" class="spritetv sprite-tnt   TNT"></a>&nbsp;&nbsp;
-			<a href="/tv/space/" class="spritetv sprite-space  Space"></a>
-			<a href="/tv/cinecanal/" class="spritetv sprite-cinecanal  Cinecanal"></a>
-			<br>
-			<a href="/tv/studiouniversal/" class="spritetv sprite-studiouniversal  Studio Universal"></a>&nbsp;&nbsp;
-			<a href="/tv/filmzone/" class="spritetv sprite-filmzone  Film Zone"></a>&nbsp;&nbsp;
-			<a href="/tv/isat/" class="spritetv sprite-isat  I-Sat"></a>
-			<br>
-			<a href="/tv/cinemax/" class="spritetv sprite-cinemax  Cinemax"></a>&nbsp;&nbsp;
-			<a href="/tv/amc/" class="spritetv sprite-amc  AMC"></a>&nbsp;&nbsp;
-			<br>
-			<a href="/tv/tcm/" class="spritetv sprite-tcm  TCM"></a>&nbsp;&nbsp;
-			<a href="/tv/golden/" class="spritetv sprite-golden  Golden Edge"></a>&nbsp;&nbsp;
-			<a href="/tv/hbo/" class="spritetv sprite-hbo  HBO"></a>
-			<a href="/tv/hboplus/" class="spritetv sprite-hboplus  HBO Plus"></a>&nbsp;&nbsp;';
-}
+$cards[1]='<a href="/tv/telefe/" class="spritetv sprite-telefe"></a>&nbsp;<a href="/tv/eltrece/" class="spritetv sprite-eltrece"></a>&nbsp;<a href="/tv/america/" class="spritetv sprite-america"></a><br><a href="/tv/tvpublica/" class="spritetv sprite-tvp"></a><a href="/tv/canal9/" class="spritetv sprite-canal9"></a>';
+$cards[2]='<a href="/tv/tn/" class="spritetv sprite-tn"></a>&nbsp;<a href="/tv/c5n/" class="spritetv sprite-c5n"></a>&nbsp;<a href="/tv/america24/" class="spritetv sprite-a24"></a><br><a href="/tv/telesur/" class="spritetv sprite-telesur"></a><a href="/tv/rt/" class="spritetv sprite-rt"></a><br><a href="/tv/cn23/" class="spritetv sprite-cn23"></a>&nbsp;<a href="/tv/cronica/" class="spritetv sprite-cronicatv"></a>&nbsp;<a href="/tv/canal26/" class="spritetv sprite-canal26"></a>';
+$cards[3]='<a href="/tv/tycsports/" class="spritetv sprite-tycsports"></a>&nbsp;<a href="/tv/dxtv/" class="spritetv sprite-dxtv"></a><a href="/tv/americasports/" class="spritetv sprite-americasports2"></a><a href="/tv/garage/" class="spritetv sprite-garage "></a><a href="/tv/directvsports/" class="spritetv sprite-directvsports DirecTV Sports"></a><br><a href="/tv/espn/" class="spritetv sprite-espntv ESPN"></a>&nbsp;<a href="/tv/espn+/" class="spritetv sprite-espnplus ESPN+"></a><br><a href="/tv/foxsports/" class="spritetv sprite-foxsports Fox Sports"></a><a href="/tv/foxsports2/" class="spritetv sprite-foxsports2 Fox Sports 2"></a>';
+$cards[4]='<a href="/tv/tec/" class="spritetv sprite-tec Tecn&oacute;polis TV"></a><a href="/tv/encuentro/" class="spritetv sprite-encuentro Encuentro"></a><br><a href="/tv/history/" class="spritetv sprite-history History"></a><a href="/tv/discovery/" class="spritetv sprite-discovery Discovery"></a><a href="/tv/natgeo/" class="spritetv sprite-natgeo Nat Geo"></a><br><a href="/tv/id/" class="spritetv sprite-id Investigation Discovery"></a><a href="/tv/trutv/" class="spritetv sprite-trutv Trutv"></a><br><a href="/tv/tlc/" class="spritetv sprite-tlc TLC"></a><a href="/tv/hyh/" class="spritetv sprite-hyh h&#38;h"></a><a href="/tv/animalplanet/" class="spritetv sprite-animalplanet Animal Planet"></a>';
+$cards[5]='<a href="/tv/magazine/" class="spritetv sprite-magazine Magazine"></a>&nbsp;&nbsp;<a href="/tv/360/" class="spritetv sprite-360tv 360TV"></a>&nbsp;&nbsp;<a href="/tv/construirtv/" class="spritetv sprite-construirtv Construir TV"></a><a href="/tv/tvuniversidad/" class="spritetv sprite-tvuniversidad TV Universidad"></a><br><a href="/tv/acuamayor/" class="spritetv sprite-acuamayor Acua Mayor"></a><a href="/tv/qm/" class="spritetv sprite-qm QM La Plata"></a>&nbsp;<a href="/tv/argentinisima/" class="spritetv sprite-argentinisima Argentin&iacute;sima"></a>&nbsp;<a href="/tv/rural/" class="spritetv sprite-rural Canal Rural"></a><a href="/tv/acuafederal/" class="spritetv sprite-acuafederal Acua Federal"></a>';
+$cards[6]='<a href="/tv/mtv/" class="spritetv sprite-mtv MTV"></a><a href="/tv/vh1/" class="spritetv sprite-vh1 VH1"></a><a href="/tv/vevo/" class="spritetv sprite-vevo VEVO"></a><a href="/tv/onetv/" class="spritetv sprite-onetv One Tv"></a><a href="/tv/rocknpop/" class="spritetv sprite-rocknpoptv Rock & Pop Se Ve"></a>';
+$cards[7]='<a href="/tv/pakapaka/" class="spritetv sprite-pakapaka Paka Paka"></a><a href="/tv/tateti/" class="spritetv sprite-tateti tateti"></a><a href="/tv/cartoon/" class="spritetv sprite-cartoon Cartoon Network"></a><br><a href="/tv/nick/" class="spritetv sprite-nick Nick"></a><a href="/tv/discoverykids/" class="spritetv sprite-discoverykids Discovery Kids"></a><a href="/tv/disney/" class="spritetv sprite-disneychannel Disney Channel"></a><a href="/tv/disneyjunior/" class="spritetv sprite-disneyjunior Disney Junior"></a><a href="/tv/disneyxd/" class="spritetv sprite-disneyxd Disney XD"></a>';
+$cards[8]='<a href="/tv/fox/" class="spritetv sprite-fox FOX"></a><a href="/tv/warner/" class="spritetv sprite-warner Warner"></a><a href="/tv/fx/" class="spritetv sprite-fx FX"></a><br><a href="/tv/ftv/" class="spritetv sprite-ftv FTV"></a><a href="/tv/foxlife/" class="spritetv sprite-foxlife Fox Life"></a><a href="/tv/e!/" class="spritetv sprite-e! E!"></a><br><a href="/tv/universal/" class="spritetv sprite-universal Universal"></a><a href="/tv/axn/" class="spritetv sprite-axn AXN"></a><a href="/tv/sony/" class="spritetv sprite-sony Sony"></a><br><a href="/tv/tbs/" class="spritetv sprite-tbs TBS Veryfunny"></a><a href="/tv/comedycentral/" class="spritetv sprite-comedycentral Comedy Central"></a><a href="/tv/syfy/" class="spritetv sprite-syfy Syfy"></a><br><a href="/tv/gourmet/" class="spritetv sprite-gourmet El Gourmet"></a><a href="/tv/ae/" class="spritetv sprite-ae A&#38;E"></a><a href="/tv/infinito/" class="spritetv sprite-infinito Infinito"></a><br><a href="/tv/glitz/" class="spritetv sprite-glitz Glitz"></a><a href="/tv/tlnovelas/" class="spritetv sprite-tlnovelas Tlnovelas"></a><a href="/tv/casaclub/" class="spritetv sprite-casaclub Casa Club"></a>';
+$cards[9]='<a href="/tv/tnt/" class="spritetv sprite-tnt TNT"></a>&nbsp;&nbsp;<a href="/tv/space/" class="spritetv sprite-space Space"></a><a href="/tv/cinecanal/" class="spritetv sprite-cinecanal Cinecanal"></a><br><a href="/tv/studiouniversal/" class="spritetv sprite-studiouniversal Studio Universal"></a>&nbsp;&nbsp;<a href="/tv/filmzone/" class="spritetv sprite-filmzone Film Zone"></a>&nbsp;&nbsp;<a href="/tv/isat/" class="spritetv sprite-isat I-Sat"></a><br><a href="/tv/cinemax/" class="spritetv sprite-cinemax Cinemax"></a>&nbsp;&nbsp;<a href="/tv/amc/" class="spritetv sprite-amc AMC"></a>&nbsp;&nbsp;<br><a href="/tv/tcm/" class="spritetv sprite-tcm TCM"></a>&nbsp;&nbsp;<a href="/tv/golden/" class="spritetv sprite-golden Golden Edge"></a>&nbsp;&nbsp;<a href="/tv/hbo/" class="spritetv sprite-hbo HBO"></a><a href="/tv/hboplus/" class="spritetv sprite-hboplus HBO Plus"></a>&nbsp;&nbsp;';}
 else{
 	header('Location: /');
 }?>
