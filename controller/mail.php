106c5bcf8673ae05071b4cb05632551aa5bf2f20
Gonzalo <gonzalocasal@gmail.com>
2016-02-21 19:04:21 -0300

    template update

--- controller/mail.php
+++ controller/mail.php
@@ -1,13 +1,11 @@
 <?php 
-	session_start(); 
-
+	session_start();
 	require("vendor/mailer/class.smtp.php");
 	require("vendor/mailer/class.phpmailer.php");
 	require_once('../controller/vendor/recaptchalib.php');
 	$secret = "6LdsSQoTAAAAALw_SUEaBaZp9r58BmVJ64NIYzX-";
 	$response = null; 
 	$reCaptcha = new ReCaptcha($secret);
-	
 	if ($_POST["g-recaptcha-response"]) {
 		$response = $reCaptcha->verifyResponse(
 		$_SERVER["REMOTE_ADDR"],
@@ -15,12 +13,10 @@
 		);
 	}
 	if ($response != null && $response->success) {
-
 	    $name    = $_SESSION["nombre"]; 
 	    $email   = $_SESSION["email"]; 
 	    $subject = $name." - Contacto";
 	    $message = $_SESSION["mensaje"]; 
-
 	    $mail = new PHPMailer();
 	    $mail->SMTPOptions = array('ssl' => array('verify_peer' => false,'verify_peer_name' => false,'allow_self_signed' => true));
 	    $mail->IsSMTP();
@@ -30,11 +26,9 @@
 	    $mail->Username = "user@gmail.com";
 	    $mail->Password = "pass";
 	    $mail->SMTPSecure = 'tls';
-
 	    $mail->From = "from@gmail.com";
 	    $mail->FromName = "Colgado.TV";
 	    $mail->AddAddress("to@gmail.com");
-
 	    $mail->IsHTML(true);
 	    $mail->Subject = $subject;
 	    $mail->Body = $mail_body ="
@@ -42,9 +36,7 @@
 	    <div><b>Nombre:  </b>".$name."<br></div>
 	    <div><b>Email:   </b>".$email."<br></div> 
 	    <div><b>Mensaje: </b>".$message."</div>";
-	   
 	    $mail->Send();
-	 	
 	 	header("Location: http://colgado.tv/");   
 	}
 	else {
@@ -52,5 +44,4 @@
 		$_SESSION["result"] ="Parese ser que usted es un robot.";
 		header("Location: http://colgado.tv/contacto/captcha.php"); 
 	}
-
 ?>
