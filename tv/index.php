428611554ad9a079246eb39363d5ff8fa394acbd
Gonzalo Casal <gonzalocasal@gmail.com>
2016-08-05 09:31:03 -0300

    new 404 error page

--- tv/index.php
+++ tv/index.php
@@ -14,9 +14,6 @@ $cards[9]='<a href="/tv/tnt" class="spritetv sprite-tnt TNT"></a>&nbsp;&nbsp;<a 
 $model=[
 'activa'=>0,
 'titulo'=>'Colgado del Cable',
-
-
-
 'descripcion'=>'Colgado del Cable es el mejor sitio web para ver TV y escuchar radio por internet en vivo las 24hs online, gratis y sin l&iacute;mites.',
 'cards'=>$cards ];
 }
