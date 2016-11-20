30febac12dd639249240ed2a6a62cc16640ef14e
Gonzalo <gonzalocasal@globallogic.com>
2016-11-20 20:06:23 -0300

    deploy

--- radios/vorterix.php
+++ radios/vorterix.php
@@ -4,6 +4,6 @@ $model=[
 'titulo'=>'Vorterix',
 'descripcion'=>'Escuchar Vorterix por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'programacion'=>'http://www.vorterix.com/',
-'player'=>'<iframe id="ciframe" src="http://www.vorterix.com/home" width="500" height="500" frameborder="0" allowfullscreen=""></iframe>'];
+'player'=>'<iframe id="ciframe" src="http://www.vorterix.com/home" width="500" height="500" frameborder="0" allowfullscreen></iframe>'];
 include_once'../controller/prepare.php';	
 ?>
