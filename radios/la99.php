44d9aef9c5155762a3ab34d079b1fc1946c626f9
Gonzalo <gonzalocasal@globallogic.com>
2016-11-21 10:08:44 -0300

    fix radio 99.1

--- radios/la99.php
+++ radios/la99.php
@@ -3,6 +3,6 @@ $model=[
 'activa'=>2, 
 'titulo'=>'La 99.1',
 'descripcion'=>'Escuchar La 99.1 por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
-'player'=>'<iframe id="ciframe" width="300" height="200" src=" http://200.63.80.26/la99/La99Vivo.html"></iframe>'];
+'player'=>'<audio src="http://cdn.instream.audio:9085/;?_=0.5325616607261212" tabindex="0" controls="true" autoplay></audio>'];
 include_once'../controller/prepare.php';	
 ?>
