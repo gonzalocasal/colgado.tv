c546b39cd8028e766efe42874f53a800e02988b5
Gonzalo <gonzalocasal@globallogic.com>
2016-11-24 06:27:51 -0300

    update TN

--- tv/tn.php
+++ tv/tn.php
@@ -5,14 +5,6 @@ $model=[
 'titulo'=>'TN',
 'descripcion'=>'Ver TN por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'programacion'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=716',
-'player'=>'<script type="text/javascript" src="/view/js/player.js"></script>
-<object type="application/x-shockwave-flash" data="http://p.jwpcdn.com/6/8/jwplayer.flash.swf" width="950" height="535" id="player_421" name="player_421">
-<param name="allowfullscreen" value="true" />
-<param name="wmode" value="opaque" /></object>
-<script type="text/javascript">
-jwplayer("player_421").setup({
-file: "http://streaming.fibercorp.com.ar/a01/ngrp:cmd_tn_vivo1-20048_all/master.m3u8",
-title: "TN - colgado.TV",width: "950",height: "535",stretching: "exactfit",autostart: "true"});
-</script>'];
+'player'=>'<iframe id="ciframe" src="http://vmf.edge-apps.net/embed/live.php?streamname=tn_live01-100083&autoplay=true" width="950" height="535" allowfullscreen></iframe>'];
 include_once'../controller/prepare.php'	;
 ?>
