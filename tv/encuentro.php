285d8ee3dd6c3094d213893b19a513adfe509313
Gonzalo <gonzalocasal@globallogic.com>
2016-11-27 08:28:47 -0300

    update encuentro

--- tv/encuentro.php
+++ tv/encuentro.php
@@ -5,6 +5,14 @@ $model=[
 'titulo'=>'Encuentro',
 'descripcion'=>'Ver encuentro por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'programacion'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=126',
-'player'=>'<iframe id="ciframe" height="437" width="582" src="http://player.theplatform.com/p/H2PcKC/G3qlkhNSAfzb" allowfullscreen></iframe>'];
+'player'=>'<script type="text/javascript" src="/view/js/player.js"></script>
+<object type="application/x-shockwave-flash" data="http://p.jwpcdn.com/6/8/jwplayer.flash.swf" width="950" height="535" id="player_421" name="player_421">
+<param name="allowfullscreen" value="true" />
+<param name="wmode" value="opaque" /></object>
+<script type="text/javascript">
+jwplayer("player_421").setup({
+file: "http://lsdencuentrohls-lh.akamaihd.net/i/encuentrohls_1@96967/master.m3u8",
+title: "Encuentro - colgado.TV",width: "950",height: "535",stretching: "exactfit",autostart: "true"});
+</script>'];
 include_once'../controller/prepare.php'	;
 ?>
