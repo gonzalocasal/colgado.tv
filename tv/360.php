f7f6d9b8b1da650b2ee316e402bdf9a1b13953c0
Gonzalo <gonzalocasal@globallogic.com>
2016-11-27 08:33:29 -0300

    update 360 tv

--- tv/360.php
+++ tv/360.php
@@ -5,6 +5,14 @@ $model=[
 'titulo'=>'360 TV',
 'descripcion'=>'Ver 360 TV por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'programacion'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=723',
-'player'=>'<iframe id="ciframe" src="http://player.theplatform.com/p/H2PcKC/q1pXcwkfHeMb/embed/select/oWTOGRoIXhoo?autoPlay=true" width="950" height="535"  allowfullscreen=""></iframe>'];
+'player'=>'<script type="text/javascript" src="/view/js/player.js"></script>
+<object type="application/x-shockwave-flash" data="http://p.jwpcdn.com/6/8/jwplayer.flash.swf" width="950" height="535" id="player_421" name="player_421">
+<param name="allowfullscreen" value="true" />
+<param name="wmode" value="opaque" /></object>
+<script type="text/javascript">
+jwplayer("player_421").setup({
+file: "http://cdn-zc-lsd-3.planisys.net:1935/TV1/smil:tv1live.smil/playlist.m3u8",
+title: "360 TV - colgado.TV",width: "950",height: "535",stretching: "exactfit",autostart: "true"});
+</script>'];
 include_once'../controller/prepare.php';
 ?>
