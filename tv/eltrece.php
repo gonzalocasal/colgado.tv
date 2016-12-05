5d90a670ddcc463093e952e05780ef28d0357ba4
Gonzalo <gonzalocasal@globallogic.com>
2016-12-05 17:41:07 -0300

    update el trece

--- tv/eltrece.php
+++ tv/eltrece.php
@@ -5,6 +5,6 @@ $model=[
 'title'=>'El Trece',
 'description'=>'Ver El Trece por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=124',
-'player'=>'<iframe id="ciframe" src="http://vaughnlive.tv/embed/video/juan_13tv?viewers=true&width=780&height=410&watermark=left&autoplay=true" width="950" height="535"></iframe>'];
+'player'=>'<iframe id="ciframe" src="http://vmf.edge-apps.net/embed/live.php?streamname=el13_live01-100083&autoplay=true" width="950" height="535" allowfullscreen></iframe>'];
 include_once'../controller/prepare.php';	
 ?>
