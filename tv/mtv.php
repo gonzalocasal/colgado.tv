109dcc1fae978ddf1fa17cc0db5c4d7b07a78595
Gonzalo <gonzalocasal@globallogic.com>
2016-12-06 09:25:23 -0300

    update VH1 & MTV

--- tv/mtv.php
+++ tv/mtv.php
@@ -4,6 +4,6 @@ $model=[
 'ad'=>true,
 'title'=>'MTV',
 'description'=>'Ver MTV por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
-'player'=>'<iframe id="ciframe" src="http://embed.latino-webtv.com/mtv.html" width="950" height="535"></iframe>'];
+'player'=>'<iframe id="ciframe" src="http://oneplay.tv/video_embed/MTV/95/100%25x100%25.html" width="950" height="535" allowfullscreen></iframe>'];
 include_once'../controller/prepare.php'	;
 ?>
--- tv/vh1.php
+++ tv/vh1.php
@@ -4,6 +4,6 @@ $model=[
 'ad'=>true,
 'title'=>'VH1',
 'description'=>'Ver VH1 por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
-'player'=>'<iframe id="ciframe" src="http://onetvgratis.com/video_embed/VHR/94/100%25x100%25.html" width="950" height="535" allowfullscreen></iframe>'];
+'player'=>'<iframe id="ciframe" src="http://oneplay.tv/video_embed/VHR/94/100%25x100%25.html" width="950" height="535" allowfullscreen></iframe>'];
 include_once'../controller/prepare.php'	;
 ?>
