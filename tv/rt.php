fb36e3ae5c158553474c4812c39a44c293580d3e
Gonzalo <gonzalocasal@globallogic.com>
2016-12-09 17:19:51 -0300

    update RT

--- tv/rt.php
+++ tv/rt.php
@@ -4,6 +4,6 @@ $model=[
 'ad'=>true,
 'title'=>'RT Noticias',
 'description'=>'Ver RT Noticias por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
-'player'=>'<iframe id="ciframe" width="950" height="535" src="http://actualidad.rt.com/static/embed/envivo.html"></iframe>'];
+'player'=>'<iframe id="ciframe" width="950" height="535" src="https://www.youtube.com/embed/OALyNX4IbBI?autoplay=1" frameborder="0" allowfullscreen></iframe>'];
 include_once'../controller/prepare.php'	;
 ?>
