f70e89919a12b3a94e8efa21ecbbc1b1ad782bac
Gonzalo <gonzalocasal@globallogic.com>
2016-12-10 18:12:52 -0300

    update garage tv

--- tv/garage.php
+++ tv/garage.php
@@ -4,7 +4,7 @@ $model=[
 'ad'=>true,
 'title'=>'El Garage TV',
 'description'=>'Ver El Garage TV por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
-'schedule'=>'http://www.ar.elgarage.com/tv/programas',
+'schedule'=>'http://www.elgarage.com/',
 'player'=>'<script type="text/javascript" src="/view/js/player.js"></script>
 <object type="application/x-shockwave-flash" data="http://p.jwpcdn.com/6/8/jwplayer.flash.swf" width="950" height="535" id="player_421" name="player_421">
 <param name="allowfullscreen" value="true" />
