db0d4c384e2a23c2f95742f7fdc1d45887b38f08
Gonzalo <gonzalocasal@globallogic.com>
2016-12-11 08:53:27 -0300

    update a24

--- tv/america24.php
+++ tv/america24.php
@@ -1,7 +1,7 @@
 <?php
 $model=[
 'active'=>'tv',
-$ad='si',
+'ad'=>'si',
 'title'=>'Am&eacute;rica 24',
 'description'=>'Ver Am&eacute;rica 24 por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=719',
