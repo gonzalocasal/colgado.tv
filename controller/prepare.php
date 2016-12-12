9ff929cafeef1cae15adb39ed7a9b7b9c5ee7ca0
Gonzalo <gonzalocasal@globallogic.com>
2016-12-12 19:27:53 -0300

    update prepare.php

--- controller/prepare.php
+++ controller/prepare.php
@@ -1,9 +1,7 @@
 <?php
-include 'config.php';
-
+require_once 'config.php';
 $environmet=['googleKey'=>getenv('GOOGLE_KEY'),'captcha'=>getenv('CAPTCHA_PUBLIC_KEY'),'version'=>getenv('VIEW_VERSION')];
 $model = array_merge($model, $environmet);
-
 $template = $twig->loadTemplate("template.html");
 $template->display($model);
 ?>
