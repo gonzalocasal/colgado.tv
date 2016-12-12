4841dc61e804172908d38a3411ed77ae74af8897
Gonzalo <gonzalocasal@globallogic.com>
2016-12-12 10:14:52 -0300

    new VIEW_VERSION environment variable

---
+++ controller/config.php
@@ -0,0 +1,10 @@
+<?php
+header("Content-Type: text/html; charset=utf-8");
+$root= $_SERVER["DOCUMENT_ROOT"];
+require_once ''.$root.'/controller/vendor/twig/lib/Twig/Autoloader.php';
+$templateDir=''.$root.'/view/';
+$templateDirCompi=''.$root.'/view/view-c';
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem($templateDir);
+$twig = new Twig_Environment($loader);
+?>
--- controller/prepare.php
+++ controller/prepare.php
@@ -1,16 +1,9 @@
 <?php
-header("Content-Type: text/html; charset=utf-8");
-$root= $_SERVER["DOCUMENT_ROOT"];
-require_once ''.$root.'/controller/vendor/twig/lib/Twig/Autoloader.php';
+include 'config.php';
 
-$templateDir=''.$root.'/view/';
-$templateDirCompi=''.$root.'/view/view-c';
-Twig_Autoloader::register();
-$loader = new Twig_Loader_Filesystem($templateDir);
-$keys=['googleKey'=>getenv('GOOGLE_KEY'),'captcha'=>getenv('CAPTCHA_PUBLIC_KEY')];
-$model = array_merge($model, $keys);
-$twig = new Twig_Environment($loader);
+$environmet=['googleKey'=>getenv('GOOGLE_KEY'),'captcha'=>getenv('CAPTCHA_PUBLIC_KEY'),'version'=>getenv('VIEW_VERSION')];
+$model = array_merge($model, $environmet);
+
 $template = $twig->loadTemplate("template.html");
-
 $template->display($model);
 ?>
