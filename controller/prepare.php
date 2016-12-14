423ee4b0d5391f36b7b2cc5200a624f5604c3023
Gonzalo <gonzalocasal@globallogic.com>
2016-12-14 09:22:11 -0300

    cache

--- controller/prepare.php
+++ controller/prepare.php
@@ -1,5 +1,8 @@
 <?php
 require_once 'config.php';
+ header("Cache-Control: no-cache, must-revalidate"); //HTTP 1.1
+  header("Pragma: no-cache"); //HTTP 1.0
+  header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 $environmet=['googleKey'=>getenv('GOOGLE_KEY'),'captcha'=>getenv('CAPTCHA_PUBLIC_KEY'),'version'=>getenv('VIEW_VERSION')];
 $model = array_merge($model, $environmet);
 $template = $twig->loadTemplate("template.html");
