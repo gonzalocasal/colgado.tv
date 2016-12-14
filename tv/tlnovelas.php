c206d3d076d694ae094ac22273b21259dcb18c1e
Gonzalo <gonzalocasal@globallogic.com>
2016-12-14 09:39:41 -0300

    cache

--- tv/tlnovelas.php
+++ tv/tlnovelas.php
@@ -5,6 +5,6 @@ $model=[
 'title'=>'Tlnovelas',
 'description'=>'Ver Tlnovelas por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=227',
-'player'=>'<iframe id="ciframe" width="950" height="535" src="http://on"></iframe>'];
+'player'=>'<iframe id="ciframe" width="950" height="535" src="http://oneplay.tv/embed/?i=64&n=Ver+canal+TLnovelas++en+vivo+Gratis&w=100%&h=100%"></iframe>'];
 include_once'../controller/prepare.php'	;
 ?>
