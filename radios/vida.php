531d5949befbfe750a7f6554937b0d9828b9544d
Gonzalo <gonzalocasal@globallogic.com>
2016-12-21 00:42:27 -0300

    update media

--- radios/vida.php
+++ radios/vida.php
@@ -4,6 +4,6 @@ $model=[
 'title'=>'Vida',
 'description'=>'Escuchar Vida por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.fmvida.com.ar/?activeSeccion_var=11',
-'player'=>'<iframe id="ciframe" width="500" height="500" src="http://www.raddios.com/application/libraries/raddios-player/player-raddios.php?rand=4845317&r=319&type=mp3&autoplay=1&currentsong=0&lang=es"></iframe>'];
+'player'=>'<iframe id="ciframe" width="500" height="50" src="http://www.raddios.com/application/libraries/raddios-player/player-raddios.php?rand=4845317&r=319&type=mp3&autoplay=1&currentsong=0&lang=es"></iframe>'];
 include_once'../controller/prepare.php';	
 ?>
