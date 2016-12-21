7c735184e52b5eab011e1ae37d43a44923b887e8
Gonzalo <gonzalocasal@globallogic.com>
2016-12-21 00:03:58 -0300

    update telefe

--- tv/telefe.php
+++ tv/telefe.php
@@ -5,6 +5,6 @@ $model=[
 'title'=>'Telefe',
 'description'=>'Ver Telefe online por internet en vivo las 24hs toda la programaci&oacute;n sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=123',
-'player'=>'<iframe id="ciframe" src="http://vaughnlive.tv/embed/video/beto_8?viewers=true&width=950&height=535&watermark=left&autoplay=true" width="950" height="535"></iframe>'];
+'player'=>'<iframe id="ciframe" src="http://vaughnlive.tv/embed/video/beto_8_?viewers=true&width=950&height=535&watermark=left&autoplay=true" width="950" height="535"></iframe>'];
 include_once'../controller/prepare.php';	
 ?>
