5c6cf6310eba88b675b81855ccba5e40bcaa299f
Gonzalo <gonzalocasal@globallogic.com>
2016-12-21 09:13:25 -0300

    tvpublica

--- tv/tvpublica.php
+++ tv/tvpublica.php
@@ -5,8 +5,8 @@ $model=[
 'title'=>'TV P&uacute;blica',
 'description'=>'Ver la TV P&uacute;blica por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=121',
-'player'=>'<div style="overflow:hidden;width:710px;height:400px">
-			<iframe id="ciframe" src="http://www.tvpublica.com.ar/vivo-2/" width="950" height="950" style="margin-top: -170px; margin-left: -120px;"></iframe>
+'player'=>'<div style="overflow:hidden;width:970px;height:544px">
+			<iframe id="ciframe" src="http://www.tvpublica.com.ar/vivo-2/" width="1000" height="1040" style="margin-top: -170px; margin-left: -15px;" allowfullscreen></iframe>
 			</div>'];
 include_once'../controller/prepare.php';	
 ?>
