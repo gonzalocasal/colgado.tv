e0e3bcf975e3cff2f095694f3e0f4dae221bc9b5
Gonzalo <gonzalocasal@globallogic.com>
2016-12-20 22:29:38 -0300

    update tv publica

--- tv/tvpublica.php
+++ tv/tvpublica.php
@@ -5,6 +5,8 @@ $model=[
 'title'=>'TV P&uacute;blica',
 'description'=>'Ver la TV P&uacute;blica por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=121',
-'player'=>'<iframe id="ciframe" src="http://vmf.edge-apps.net/embed/live.php?streamname=c7live01-20034&autoplay=true" width="950" height="535" allowfullscreen></iframe>'];
+'player'=>'<div style="overflow:hidden;width:710px;height:400px">
+			<iframe id="ciframe" src="http://www.tvpublica.com.ar/vivo-2/" width="950" height="950" style="margin-top: -170px; margin-left: -120px;"></iframe>
+			</div>'];
 include_once'../controller/prepare.php';	
 ?>
