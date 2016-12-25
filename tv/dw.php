1dfc34ccf00c7344be3c781a90d7d10f000b38d6
Gonzalo <gonzalocasal@globallogic.com>
2016-12-24 22:06:53 -0300

    media logos & css updates

---
+++ tv/dw.php
@@ -0,0 +1,9 @@
+<?php
+$model=[
+'active'=>'tv',
+'ad'=>true,
+'title'=>'Deutsche Welle',
+'description'=>'Ver Deutsche Welle Noticias por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
+'player'=>'<iframe id="ciframe" width="950" height="535" src="https://www.youtube.com/embed/wBDQlzVRMgU?autoplay=1" frameborder="0" allowfullscreen></iframe>'];
+include_once'../controller/prepare.php'	;
+?>
