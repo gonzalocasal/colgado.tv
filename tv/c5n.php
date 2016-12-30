5be142126b0242db1ea96f261b879cf5784af8aa
Gonzalo <gonzalocasal@globallogic.com>
2016-12-29 22:19:27 -0300

    update media

--- tv/c5n.php
+++ tv/c5n.php
@@ -5,6 +5,6 @@ $model=[
 'title'=>'C5N',
 'description'=>'Ver C5N internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=717',
-'player'=>'<iframe id="ciframe" width="950" height="535" src="https://www.youtube.com/embed/VtEX_jf7uVA?autoplay=1" frameborder="0" allowfullscreen></iframe>'];
+'player'=>'<iframe id="ciframe" width="950" height="535" src="https://www.youtube.com/embed/enYoqgpk3mg?autoplay=1" frameborder="0" allowfullscreen></iframe>'];
 include_once'../controller/prepare.php'	;
 ?>
