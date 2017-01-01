42264e13499953e135bf43ab489dccf4176e8d18
Gonzalo <gonzalocasal@globallogic.com>
2017-01-01 12:17:44 -0300

    update cn23

--- tv/cn23.php
+++ tv/cn23.php
@@ -4,6 +4,6 @@ $model=[
 'ad'=>true,
 'title'=>'CN23',
 'description'=>'Ver CN23 internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
-'player'=>'<iframe id="ciframe" width="950" height="535" src="https://www.youtube.com/embed/hOj1cq9udrc?autoplay=1" frameborder="0" allowfullscreen></iframe>'];
+'player'=>'<iframe id="ciframe" width="950" height="535" src="https://www.youtube.com/embed/SvfGD8asiKY?autoplay=1" frameborder="0" allowfullscreen></iframe>'];
 include_once'../controller/prepare.php'	;
 ?>
