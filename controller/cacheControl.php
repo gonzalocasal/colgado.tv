ef5d07c98def8f8292a3122a1120984b6f608ff1
Gonzalo <gonzalocasal@globallogic.com>
2017-01-08 12:29:47 -0300

    cache control

--- controller/cacheControl.php
+++ controller/cacheControl.php
@@ -1,15 +1,9 @@
 <?php
 $environment = getenv('VIEW_VERSION');
 $parameter= $_GET['v'];
-if (array_key_exists('player', $model) || (( $environment != $parameter)) ){
+if (array_key_exists('player', $model)) ){
 	header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
 	header("Pragma: no-cache"); // HTTP 1.0.
 	header("Expires: 0");
 }
-
-echo "environment:";
-var_dump($environment);
-echo "param;";	
-var_dump($parameter);
-
 ?>
