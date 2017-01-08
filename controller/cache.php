b02fa57b9709d67fa180c8884bf579d9896f68be
Gonzalo <gonzalocasal@globallogic.com>
2017-01-08 19:44:41 -0300

    cache control

--- controller/cache.php
+++ controller/cache.php
@@ -1,7 +1,10 @@
 <?php
-if (array_key_exists('player', $model) ){
+if (isMedia()){
 	header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
 	header("Pragma: no-cache"); // HTTP 1.0.
 	header("Expires: 0");
+}
+function isMedia(){
+	return array_key_exists('player', $model);
 }
 ?>
