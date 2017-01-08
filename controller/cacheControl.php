9973c849eb3c456fc6cbc9a5880d17a56806ce88
Gonzalo <gonzalocasal@globallogic.com>
2017-01-08 19:35:12 -0300

    cache control

--- controller/cacheControl.php
+++ controller/cacheControl.php
@@ -1,14 +1,5 @@
 <?php
-$version = getenv('VIEW_VERSION');
-if(!isset($_COOKIE['version'])) {
-    setcookie("version", $version, time()+3600);
-    $clientVersion = "noVersion";
-} else {
-    $clientVersion = $_COOKIE['version'];
-}
-echo "COOKIE:";
-var_dump($_COOKIE['version']);
-if (array_key_exists('player', $model) || ($version != $clientVersion)){
+if (array_key_exists('player', $model) ){
 	header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
 	header("Pragma: no-cache"); // HTTP 1.0.
 	header("Expires: 0");
