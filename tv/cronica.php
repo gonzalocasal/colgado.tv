527b4e9ad83d9e13e9a88019e961488afafd165b
Gonzalo <gonzalocasal@globallogic.com>
2017-01-22 18:51:44 -0300

    update media

--- tv/cronica.php
+++ tv/cronica.php
@@ -5,6 +5,6 @@ $model=[
 'title'=>'Cr&oacute;nica TV',
 'description'=>'Ver Cr&oacute;nica TV por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=718',
-'player'=>'<iframe id="ciframe" src="http://vmf.edge-apps.net/embed/live.php?streamname=cronicatv_video1-100044&autoplay=true" width="950" height="535"></iframe>'];
+'player'=>'<iframe id="ciframe" width="950" height="535" src="https://www.youtube.com/embed/5UfTmvIYp4c?autoplay=1" frameborder="0" allowfullscreen></iframe>'];
 include_once'../controller/prepare.php';	
 ?>
--- tv/tlc.php
+++ tv/tlc.php
@@ -5,6 +5,6 @@ $model=[
 'title'=>'TLC',
 'description'=>'Ver TLC por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=740',
-'player'=>'<iframe id="ciframe" src="http://embed.latino-webtv.com/tlc.html" width="950" height="535"></iframe>'];
+'player'=>'<iframe id="ciframe" src="http://latino-webtv.com/embed/canales.php?ch=386&name=TLC&hd=no" width="950" height="535"></iframe>'];
 include_once'../controller/prepare.php'	;
 ?>
