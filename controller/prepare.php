4c058c6673c695d6a16db0adc62fe862f6620117
Gonzalo <gonzalocasal@globallogic.com>
2017-01-23 10:07:53 -0300

    upate media

--- controller/prepare.php
+++ controller/prepare.php
@@ -1,7 +1,10 @@
 <?php
 require 'cache.php';
 require 'config.php';
-$environmet=['googleKey'=>getenv('GOOGLE_KEY'),'captcha'=>getenv('CAPTCHA_PUBLIC_KEY'),'version'=>getenv('VIEW_VERSION')];
+$environmet=[
+'googleKey'=>getenv('GOOGLE_KEY'),
+'captcha'=>getenv('CAPTCHA_PUBLIC_KEY'),
+'version'=>getenv('VIEW_VERSION')];
 $model = array_merge($model, $environmet);
 $template = $twig->loadTemplate("template.html");
 $template->display($model);
