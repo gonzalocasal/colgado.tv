6a3aed3d0ece4fd85a8e069e618dbaa6c7caada5
Gonzalo <gonzalocasal@globallogic.com>
2017-01-23 07:38:54 -0300

    update media

--- tv/america.php
+++ tv/america.php
@@ -5,6 +5,6 @@ $model=[
 'title'=>'Am&eacute;rica',
 'description'=>'Ver Am&eacute;rica por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=120',
-'player'=>'<iframe id="ciframe" src="http://vaughnlive.tv/embed/video/el_yamato?viewers=true&width=660&height=400&watermark=falset&autoplay=true" width="950" height="535"></iframe>'];
+'player'=>'<iframe id="ciframe" src="http://vmf.edge-apps.net/embed/live.php?streamname=americatvlive-100056&autoplay" width="950" height="535" allowfullscreen></iframe>'];
 include_once'../controller/prepare.php'	;
 ?>
