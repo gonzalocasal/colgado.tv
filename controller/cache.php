dc16ae35ada496df573a8a2f9b81b7bf1be0e738
Gonzalo <gonzalocasal@globallogic.com>
2017-01-23 10:21:13 -0300

    update cache config

--- controller/cache.php
+++ controller/cache.php
@@ -1,11 +1,5 @@
 <?php
-//if (isMedia()){
-if (true){
-	header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
-	header("Pragma: no-cache"); // HTTP 1.0.
-	header("Expires: 0");
-}
-function isMedia(){
-	return array_key_exists('player', $model);
-}
+header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+header("Pragma: no-cache"); // HTTP 1.0.
+header("Expires: 0");
 ?>
