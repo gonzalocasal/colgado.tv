bbaa350efc4c9be79f13a8a7d7a34590f60de14d
Gonzalo <gonzalocasal@globallogic.com>
2017-01-23 10:31:26 -0300

    update cache config

--- controller/cache.php
+++ controller/cache.php
@@ -1,5 +1,7 @@
 <?php
-header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
-header("Pragma: no-cache"); // HTTP 1.0.
-header("Expires: 0");
+if (array_key_exists('player', $model)){
+	header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+	header("Pragma: no-cache"); // HTTP 1.0.
+	header("Expires: 0");
+}
 ?>
--- controller/prepare.php
+++ controller/prepare.php
@@ -5,9 +5,6 @@ $environmet=[
 'googleKey'=>getenv('GOOGLE_KEY'),
 'captcha'=>getenv('CAPTCHA_PUBLIC_KEY'),
 'version'=>getenv('VIEW_VERSION')];
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
 $model = array_merge($model, $environmet);
 $template = $twig->loadTemplate("template.html");
 $template->display($model);
