6e2b3cc7cc63d786f93d22633f2a931c3602ee8a
Gonzalo <gonzalocasal@globallogic.com>
2017-01-26 11:06:26 -0300

    update radio del plata

--- radios/delplata.php
+++ radios/delplata.php
@@ -4,6 +4,6 @@ $model=[
 'title'=>'Del Plata',
 'description'=>'Escuchar Del Plata por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.amdelplata.com/index.php?option=com_grillaprogramacion&view=grillaprogramacion&days=lv&Itemid=14',
-'player'=>'<iframe style="border:none!important;height:30px;width:280px;margin:0 40px" src="http://latinstatic.edgesuite.net/delplata/" allowtransparency="true" class="style-scope my-player"></iframe>'];
+'player'=>'<iframe style="border:none!important;height:30px;width:280px;margin:0 40px" src="http://www.alsolnet.com/stream/amdelplata/" allowtransparency="true" class="style-scope my-player"></iframe>'];
 include_once'../controller/prepare.php';	
 ?>
