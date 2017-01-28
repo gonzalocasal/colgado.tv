d86e5493c843be0b893e1b8626f1277b190fa971
Gonzalo <gonzalocasal@globallogic.com>
2017-01-27 21:09:59 -0300

    update radios & logos

---
+++ radios/am950.php
@@ -0,0 +1,9 @@
+<?php
+$model=[
+'active'=>'radios',
+'title'=>'AM950 Radio Belgrano',
+'description'=>'Escuchar AM950 Radio Belgrano por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
+'schedule'=>'http://www.am950belgrano.com/programacion/',
+'player'=>'<embed src="http://www.alsolnet.com/stream/radio9/alsolnethd.swf" flashvars="audioid=radio9" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="358" height="240">'];
+include_once'../controller/prepare.php';
+?>
