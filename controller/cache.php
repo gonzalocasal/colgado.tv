a2f995c7581783596936fd4c9834368619d9e7ae
Gonzalo <gonzalocasal@globallogic.com>
2017-01-28 09:10:58 -0300

    view version cookie

--- controller/cache.php
+++ controller/cache.php
@@ -1,10 +1,7 @@
 <?php
-$version = getenv('VIEW_VERSION');
-$update = ( isset($_COOKIE["VIEW_VERSION"]) && $_COOKIE["VIEW_VERSION"] == $version);
-if (array_key_exists('player', $model) || $update){
+if (array_key_exists('player', $model)){
 	header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
 	header("Pragma: no-cache"); // HTTP 1.0.
 	header("Expires: 0");
-	setcookie("VIEW_VERSION", $version, time() + 604800);
 }
 ?>
