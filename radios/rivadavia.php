5850d2393889d996cd6a549daf3d7aea3b31525c
Gonzalo <gonzalocasal@globallogic.com>
2017-01-29 14:12:03 -0300

    update rivadavia

--- radios/rivadavia.php
+++ radios/rivadavia.php
@@ -4,6 +4,6 @@ $model=[
 'title'=>'Rivadavia',
 'description'=>'Escuchar Rivadavia por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://rivadavia.com.ar/programacion/',
-'player'=>'<audio controls="controls" autoplay="autoplay" src="http://190.104.220.205:8234/stream?type=.mp3"></audio>'];
+'player'=>'<audio controls="controls" autoplay="autoplay" src="http://38.107.243.197:9639/live"></audio>'];
 include_once'../controller/prepare.php';	
 ?>
