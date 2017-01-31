1bed0e9fd1c77e8e73da1fd12b7d74dfc173e564
Gonzalo <gonzalocasal@globallogic.com>
2017-01-31 14:32:45 -0300

    update media

--- tv/c5n.php
+++ tv/c5n.php
@@ -5,6 +5,14 @@ $model=[
 'title'=>'C5N',
 'description'=>'Ver C5N internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=717',
-'player'=>'<iframe id="ciframe" width="950" height="535" src="https://www.youtube.com/embed/JSiJTHY22-Q?autoplay=1" frameborder="0" allowfullscreen></iframe>'];
+'player'=>'<script type="text/javascript" src="/view/js/player.js"></script>
+<object type="application/x-shockwave-flash" data="http://p.jwpcdn.com/6/8/jwplayer.flash.swf" width="950" height="535" id="player_421" name="player_421">
+<param name="allowfullscreen" value="true" />
+<param name="wmode" value="opaque" /></object>
+<script type="text/javascript">
+jwplayer("player_421").setup({
+file: "http://server3.stweb.tv/c5n/ngrp:live_all/chunklist_w1452346085_b1634304.m3u8",
+title: "C5N - colgado.TV",width: "950",height: "535",stretching: "exactfit",autostart: "true"});
+</script>'];
 include_once'../controller/prepare.php'	;
 ?>
