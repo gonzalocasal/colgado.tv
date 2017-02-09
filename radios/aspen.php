d1cc9fb1651ce3048e82781acf5bba562011eb6b
Gonzalo <gonzalocasal@globallogic.com>
2017-02-09 10:36:48 -0300

    update radio aspen

--- radios/aspen.php
+++ radios/aspen.php
@@ -4,6 +4,6 @@ $model=[
 'title'=>'Aspen',
 'description'=>'Escuchar Aspen por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.fmaspen.com/programacion.php',
-'player'=>'<audio src="http://19493.live.streamtheworld.com/ASPENAAC.aac?csegid=2000&tdsdk=js-2.9&pname=TDSdk&pversion=2.9&banners=300x250&streamtheworld_user=1&sbmid=abf19801-b2b6-4bd9-9b7a-9f63a36ed001" tabindex="0" controls autoplay></audio>'];
+'player'=>'<audio src="http://17973.live.streamtheworld.com/ASPENAAC.aac" controls autoplay></audio>'];
 include_once'../controller/prepare.php';	
 ?>
