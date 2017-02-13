9a03f308cf55016e2cfdb418d5b6deabd7c42583
Gonzalo <gonzalocasal@globallogic.com>
2017-02-13 20:20:50 -0300

    update scripts

--- view/js/scripts.js
+++ view/js/scripts.js
@@ -11,7 +11,15 @@ $(document).ready(function(a) {
     }
     //REMOVE SCROLLBAR FROM PLAYER
     $("#ciframe").attr("scrolling", "no");
-});
+
+    //ANALYTICS
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-41210250-1', 'auto');
+    ga('send', 'pageview');
+    });
 
 //CLOSE AD
 $("#close-ad").click(function() {
@@ -126,10 +134,3 @@ function(a) {
     });
 }
 
-//ANALYTICS
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-41210250-1', 'auto');
-ga('send', 'pageview');
