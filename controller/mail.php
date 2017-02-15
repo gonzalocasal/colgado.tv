45462fdc150bb0108c23d16db0327362914dc6d0
Gonzalo <gonzalocasal@globallogic.com>
2017-02-15 11:47:19 -0300

    update mail.php

--- controller/mail.php
+++ controller/mail.php
@@ -1,39 +1,27 @@
 <?php 
-
 session_start(); 
 require('vendor/sendgrid/sendgrid-php.php');
 require('vendor/recaptchalib.php');
 
 $secret = getenv('CAPTCHA_SECRET_KEY');
-$response = null; 
 $reCaptcha = new ReCaptcha($secret);
 
 if ($_POST["g-recaptcha-response"]) {
 	$response = $reCaptcha->verifyResponse($_SERVER["REMOTE_ADDR"], $_POST["g-recaptcha-response"]);
 }
 
-if ($response != null && $response->success) {
-
-    $name    = $_SESSION["name"]; 
-    $email   = $_SESSION["email"]; 
-    $message = $_SESSION["message"];
-    $receiver= getenv('EMAIL_RECEIVER');
-
-	$from = new SendGrid\Email(null, $email);
-	$subject = "contacto de ".$name;
-	$to = new SendGrid\Email(null, $receiver);
-	$content = new SendGrid\Content("text/plain",$message);
+if ( isset($response) != null && $response->success) {
+   	$subject = "contacto de ".$_SESSION["name"];
+	$from = new SendGrid\Email(null, $_SESSION["email"]);
+	$to = new SendGrid\Email(null, getenv('EMAIL_RECEIVER'));
+	$content = new SendGrid\Content("text/plain",$_SESSION["message"]);
 	$mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-	$apiKey = getenv('SENDGRID_API_KEY');
-	$sg = new \SendGrid($apiKey);
-
+	$sg = new \SendGrid(getenv('SENDGRID_API_KEY'));
 	$response = $sg->client->mail()->send()->post($mail);
-
 	session_destroy();
 	require('../contacto/mensaje.php');
 }else{
 	header("Location:".$_SERVER["DOCUMENT_ROOT"]."");
 }
-
 ?>
