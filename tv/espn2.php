b92cc639bf0ca889b4901d6621a7d7a36aa39214
Gonzalo <gonzalocasal@globallogic.com>
2017-02-15 14:48:55 -0300

    update espn2

--- tv/espn2.php
+++ tv/espn2.php
@@ -6,6 +6,6 @@ $model=[
 'title'=>'ESPN 2',
 'description'=>'Ver ESPN 2 por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=623',
-'player'=>'iframe id="ciframe" src="http://tvpor-internet.com/Espn-en-vivo.html" width="600" height="400" allowFullscreen></iframe>'];
+'player'=>'<iframe id="ciframe" src="http://tvpor-internet.com/espn-2-en-vivo.html" width="600" height="400" allowFullscreen></iframe>'];
 include_once'../controller/prepare.php'	;
 ?>
