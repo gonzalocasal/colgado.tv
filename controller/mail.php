d06e72c21f51bedf5efb8b9a9d4bd0aae7c95a07
Gonzalo <gonzalocasal@globallogic.com>
2017-02-17 11:49:50 -0300

    update mail.php

--- controller/mail.php
+++ controller/mail.php
@@ -12,10 +12,10 @@ if ($_POST["g-recaptcha-response"]) {
 
 if ( isset($response) != null && $response->success) {
    	$subject = "contacto de ".$_SESSION["name"];
-	$from = new SendGrid\Email(null, $_SESSION["email"]);
-	$to = new SendGrid\Email(null, getenv('EMAIL_RECEIVER'));
+	$from    = new SendGrid\Email(null, $_SESSION["email"]);
+	$to      = new SendGrid\Email(null, getenv('EMAIL_RECEIVER'));
 	$content = new SendGrid\Content("text/plain",$_SESSION["message"]);
-	$mail = new SendGrid\Mail($from, $subject, $to, $content);
+	$mail    = new SendGrid\Mail($from, $subject, $to, $content);
 
 	$sg = new \SendGrid(getenv('SENDGRID_API_KEY'));
 	$response = $sg->client->mail()->send()->post($mail);
