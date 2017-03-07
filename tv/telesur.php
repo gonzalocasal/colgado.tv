5de12c81de59fa3a67efc4fb0c343cfd927a5939
Gonzalo <gonzalocasal@gmail.com>
2017-03-07 12:12:12 -0300

    update media

--- tv/telesur.php
+++ tv/telesur.php
@@ -5,6 +5,6 @@ $model=[
 'title'=>'Telesur',
 'description'=>'Ver Telesur por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=722',
-'player'=>'<iframe id="ciframe" width="950" height="535" src="https://www.youtube.com/embed/3fwfmhTYUZs?autoplay=1" frameborder="0" allowfullscreen></iframe>'];
+'player'=>'<iframe id="ciframe" width="950" height="535" src="https://www.youtube.com/embed/Riz5tucnZu8?autoplay=1" frameborder="0" allowfullscreen></iframe>'];
 include_once'../controller/prepare.php'	;
 ?>
