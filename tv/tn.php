5dffd993cdf0071ec0e66acb605339f7376afac5
Gonzalo <gonzalocasal@gmail.com>
2017-03-19 19:03:00 -0300

    update media

--- tv/tn.php
+++ tv/tn.php
@@ -5,6 +5,6 @@ $model=[
 'title'=>'TN',
 'description'=>'Ver TN por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=716',
-'player'=>'<iframe id="ciframe" src="http://vmf.edge-apps.net/embed/live.php?streamname=tn_live01-100083&autoplay=true" width="950" height="535" allowfullscreen></iframe>'];
+'player'=>'<iframe id="ciframe" width="950" height="535" src="https://www.youtube.com/embed/vSaguIoqr-c?autoplay=1" frameborder="0" allowfullscreen></iframe>'];
 include_once'../controller/prepare.php'	;
 ?>
