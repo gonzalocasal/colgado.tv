837950116de2392e0ccf9302d63ba3a83827a57f
Gonzalo <gonzalocasal@gmail.com>
2017-04-07 11:34:27 -0300

    update media

--- tv/cn23.php
+++ tv/cn23.php
@@ -4,6 +4,6 @@ $model=[
 'ad'=>true,
 'title'=>'CN23',
 'description'=>'Ver CN23 internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
-'player'=>'<iframe id="ciframe" width="950" height="535" src="https://www.youtube.com/embed/kD7QPLKS6iE?autoplay=1" frameborder="0" allowfullscreen></iframe>'];
+'player'=>'<iframe id="ciframe" width="950" height="535" src="https://www.youtube.com/embed/zJMo22y85kA?autoplay=1" frameborder="0" allowfullscreen></iframe>'];
 include_once'../controller/prepare.php'	;
 ?>
