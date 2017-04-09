e85c650ba44ba915f39ba2d6f28d449ff5713bf9
Gonzalo <gonzalocasal@gmail.com>
2017-04-09 13:37:13 -0300

    update media

--- tv/eltrece.php
+++ tv/eltrece.php
@@ -5,6 +5,8 @@ $model=[
 'title'=>'El Trece',
 'description'=>'Ver El Trece por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=124',
-'player'=>'<iframe id="ciframe" src="http://vaughnlive.tv/embed/video/juan_13tv?viewers=true&width=950&height=535&watermark=left&autoplay=true" width="950" height="535" allowfullscreen></iframe>'];
+'player'=>'	<div style="overflow:hidden;width:930px;height:520px">
+			<iframe id="ciframe"  width="1950" height="1535" src="http://vaughnlive.tv/juan_13tv#videoTitle"style="margin-top: -50px; margin-left: -335px;"></iframe>
+			</div>'];
 include_once'../controller/prepare.php';	
 ?>
