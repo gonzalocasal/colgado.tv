e004e175397278d954695378a818b948dfc37de1
Gonzalo <gonzalocasal@gmail.com>
2017-04-14 14:39:00 -0300

    update media

---
+++ tv/cnn.php
@@ -0,0 +1,10 @@
+<?php
+$model=[
+'active'=>'tv',
+'ad'=>true,
+'title'=>'CNN en Espa&ntilde;ol',
+'schedule'=>'http://lnmas.lanacion.com.ar/',
+'description'=>'Ver CNN en Espa&ntilde;ol por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
+'player'=>'<iframe id="ciframe" width="950" height="535" src="https://www.youtube.com/embed/I0scGq8USoA?autoplay=1" frameborder="0" allowfullscreen></iframe>'];
+include_once'../controller/prepare.php'	;
+?>
