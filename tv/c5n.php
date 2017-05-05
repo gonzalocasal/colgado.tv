ccc7e1a67a420838149799a3110c5ddeea9d66e4
gonzalocasal <gonzalocasal@gmail.com>
2017-05-05 08:26:56 -0300

    update media

--- tv/c5n.php
+++ tv/c5n.php
@@ -5,6 +5,6 @@ $model=[
 'title'=>'C5N',
 'description'=>'Ver C5N internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=717',
-'player'=>'<iframe id="ciframe" width="950" height="535" src="https://www.youtube.com/embed/_u2m4kWvk1g?autoplay=1" frameborder="0" allowfullscreen></iframe>'];
+'player'=>'<iframe id="ciframe" width="950" height="535" src="https://www.youtube.com/embed/Hopfc6lxP-c?autoplay=1" frameborder="0" allowfullscreen></iframe>'];
 include_once'../controller/prepare.php'	;
 ?>
