1a5ba4b811820e0731790ac3abf4f26ad3979ed3
gonzalocasal <gonzalocasal@gmail.com>
2017-06-04 08:00:59 -0300

    update event

--- controller/event.php
+++ controller/event.php
@@ -1,10 +1,10 @@
 <?php
 $event=[
-'eventTitle'=>'UEFA CHAMPIONS LEAGUE',
+'eventTitle'=>'CHAMPIONS LEAGUE',
 'eventDescription'=>'Ver La Final de la Champions League en vivo por internet.',
 'banner'=>'https://seeklogo.com/images/U/UEFA_Champions_League-logo-300A743B55-seeklogo.com.png',
-'date'=>'03/06/2017',
-'begin'=>'14:40',
+'date'=>'04/06/2017',
+'begin'=>'08:04',
 'ends'=>'19:00'
 ];
 ?>
