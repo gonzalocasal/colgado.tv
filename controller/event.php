41462bf5c9bbfcf4c3a4afb5c30621ea09d7827e
gonzalocasal <gonzalocasal@gmail.com>
2017-06-13 09:47:58 -0300

    update event

--- controller/event.php
+++ controller/event.php
@@ -1,6 +1,6 @@
 <?php
 $event=[
-'title'=>'Argentina - Singapur',
+'title'=>'ARGENTINA - SINGAPUR',
 'banner'=>'http://lh4.googleusercontent.com/-jYjhxTAjrjk/TkfejhTS58I/AAAAAAAAAzg/irQCYyk_eZI/logoafa.png',
 'date'=>'13/06/2017',
 'begin'=>'08:00',
