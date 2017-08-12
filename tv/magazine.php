d89cc20104b15f42d22d861afb9283c62db23c76
Gonzalo Casal <gonzalocasal@gmail.com>
2017-08-12 11:32:46 -0300

    update logos style

--- tv/magazine.php
+++ tv/magazine.php
@@ -2,7 +2,7 @@
 $model=[
 'active'=>'tv',
 'ad'=>true,
-'title'=>'Magazine',
+'title'=>'Ciudad Magazine',
 'description'=>'Ver Magazine por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=236',
 'player'=> basename($_SERVER["PHP_SELF"], ".php")];
