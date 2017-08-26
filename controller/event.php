31d995eaacc4af1ad84e90db40e3675ec3e43f01
Gonzalo Casal <gonzalocasal@gmail.com>
2017-08-26 19:42:58 -0300

    evento

--- controller/event.php
+++ controller/event.php
@@ -1,6 +1,6 @@
 <?php
 $event=[
-'title'=>'FLOYD MAYWEATHER vs. CONOR MCGREGOR',
+'title'=>'FLOYD MAYWEATHER VS CONOR MCGREGOR',
 'banner'=>'http://www.infobae.com/new-resizer/PqOI29jOJZMiV4otGjqhkAB0v5w=/600x0/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2017/07/14145028/McGregor-Mayweather-cara-a-cara-1920.jpg?token=bar',
 'beginDate'=>'26/08/2017 18:00',
 'endDate'=>'27/08/2017 02:00'
