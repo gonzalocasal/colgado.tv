64365abd13d9e7f99d714e6ad25541e8659a0589
Gonzalo Casal <gonzalocasal@gmail.com>
2017-08-26 19:15:08 -0300

    evento

--- controller/event.php
+++ controller/event.php
@@ -1,9 +1,8 @@
 <?php
 $event=[
-'title'=>'FINAL DE LA COPA CONFEDERACIONES',
-'banner'=>'http://www.indumotoraone.cl/landing/copa-confederaciones/imagenes/logo_copa.png',
-'date'=>'02/07/2017',
-'begin'=>'12:00',
-'ends'=>'18:00'
+'title'=>'FLOYD MAYWEATHER vs. CONOR MCGREGOR',
+'banner'=>'http://www.infobae.com/new-resizer/PqOI29jOJZMiV4otGjqhkAB0v5w=/600x0/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2017/07/14145028/McGregor-Mayweather-cara-a-cara-1920.jpg?token=bar',
+'beginDate'=>'26/08/2017 18:00',
+'endDate'=>'27/08/2017 02:00'
 ];
 ?>
--- controller/schedule.php
+++ controller/schedule.php
@@ -3,10 +3,8 @@ include 'event.php';
 $format = 'd/m/Y H:i';
 $dateBsAs = new DateTime("now", new DateTimeZone("America/Argentina/Buenos_Aires"));
 $currentDate = date_format($dateBsAs, $format);
-$dateBegin = $event['date'].' '.$event['begin'];
-$dateEnd   = $event['date'].' '.$event['ends'];
-$dateBegin   = \DateTime::createFromFormat($format,$dateBegin);
-$dateEnd     = \DateTime::createFromFormat($format,$dateEnd);
+$dateBegin   = \DateTime::createFromFormat($format,$event['beginDate']);
+$dateEnd     = \DateTime::createFromFormat($format,$event['endDate']);
 $currentDate = \DateTime::createFromFormat($format,$currentDate);
 $model['EVENT'] = ($dateBegin < $currentDate && $currentDate < $dateEnd? $event : false);
 ?>
