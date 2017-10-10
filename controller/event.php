be05592c042189544ea689dc287d7d06a0d80c59
Gonzalo Casal <gonzalocasal@gmail.com>
2017-10-10 18:25:40 -0300

    update evento

--- controller/event.php
+++ controller/event.php
@@ -1,8 +1,8 @@
 <?php
 $event=[
-'title'=>'ARGENTINA VS PERU',
-'banner'=>'http://www.apurogol.net/wp-content/uploads/2016/10/peru-vs-argentina-en-vivo-online-eliminatorias-sudamericanas-mundial-rusia-2018-en-directo.png',
-'beginDate'=>'05/10/2017 18:00',
-'endDate'=>'05/10/2017 23:00'
+'title'=>'ECUADOR - ARGENTINA',
+'banner'=>'/view/imgs/evento.png',
+'beginDate'=>'10/10/2017 18:00',
+'endDate'=>'10/10/2017 23:00'
 ];
 ?>
