54f454be7646374c9b115ce5510b797baa4a5e4a
Gonzalo Casal <gonzalocasal@gmail.com>
2017-11-18 20:14:28 -0300

    update scripts

--- view/js/scripts.js
+++ view/js/scripts.js
@@ -19,18 +19,13 @@ $(document).ready(function(a) {
     //REMOVE SCROLLBAR FROM PLAYER
     $("#ciframe").attr("scrolling", "no");
 
-    //ANALYTICS
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-41210250-1', 'auto');
-    ga('send', 'pageview');
+  
 });
 
 //REMOVE LOCAL CACHE ON NEW VERSION OR EVENT
 if ( (typeof localStorage.VERSION != 'undefined' && localStorage.VERSION != VERSION ) || 
-     (typeof localStorage.VERSION != 'undefined' && localStorage.EVENT !== EVENT && ACTIVE =="tv") ) {
+     (typeof localStorage.VERSION == 'undefined') ||
+     (localStorage.EVENT !== EVENT && ACTIVE =="tv") ) {
     localStorage.setItem("VERSION", VERSION);
     localStorage.setItem("EVENT", EVENT);
     window.location.reload(true);
