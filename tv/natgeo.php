7da3159aa4e9a8d36fd366b929de1e381fb8d1bf
Gonzalo Casal <gonzalocasal@gmail.com>
2017-12-18 07:09:31 -0300

    update media

--- tv/natgeo.php
+++ tv/natgeo.php
@@ -2,7 +2,7 @@
 $model=[
 'active'=>'tv',
 'ad'=>true,
-'protected'=>true,
+'protected'=>false,
 'title'=>'Nat Geo',
 'description'=>'Ver Nat Geo por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=730',
