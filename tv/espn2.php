bdb8580e083549d63d6d68caee33ab732e120dbe
Gonzalo Casal <gonzalocasal@gmail.com>
2017-12-21 21:22:50 -0300

    update media

--- tv/espn2.php
+++ tv/espn2.php
@@ -2,7 +2,7 @@
 $model=[
 'active'=>'tv',
 'ad'=>true,
-'protected'=>true,
+'protected'=>false,
 'title'=>'ESPN 2',
 'description'=>'Ver ESPN 2 por internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=623',
