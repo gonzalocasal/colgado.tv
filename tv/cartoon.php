47350583ad4573cb2c31b35a318c9a8029029cf1
Gonzalo Casal <gonzalocasal@gmail.com>
2017-12-22 06:34:53 -0300

    update media

--- tv/cartoon.php
+++ tv/cartoon.php
@@ -2,7 +2,7 @@
 $model=[
 'active'=>'tv',
 'ad'=>true,
-'protected'=>true,
+'protected'=>false,
 'title'=>'Cartoon',
 'description'=>'Ver Cartoon internet en vivo las 24hs toda la programaci&oacute;n online y sin l&iacute;mites.',
 'schedule'=>'http://www.directv.com.ar/guia/ChannelDetail.aspx?id=304',
