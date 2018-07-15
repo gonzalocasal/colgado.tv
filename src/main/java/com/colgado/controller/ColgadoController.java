93c67819a026bdf51eb4ec866dacb6f273ececbc
Gonzalo <gonzalocasal@gmail.com>
2018-07-15 11:32:14 -0300

    refactor web page controllers

--- src/main/java/com/colgado/controller/ColgadoController.java
+++ src/main/java/com/colgado/controller/ColgadoController.java
@@ -2,7 +2,7 @@ package com.colgado.controller;
 
 import org.springframework.ui.Model;
 
-public interface ControllerI {
+public interface ColgadoController {
 	
 	void addCommonAttributes(Model model);
 	void addMediaAttributes(Model model, String title, String template, String schedule);
--- src/main/java/com/colgado/controller/RadiosController.java
+++ src/main/java/com/colgado/controller/RadiosController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.colgado.service.YoutubeIdsProvider;
 
 @Controller
-public class RadiosController implements ControllerI {
+public class RadiosController implements ColgadoController {
 	
 	@Autowired
 	private YoutubeIdsProvider youtube;
