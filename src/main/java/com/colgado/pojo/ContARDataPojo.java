9b2ddbbfadfb08ea653928d73c56c5a5801b8bbd
Gonzalo <gonzalocasal@gmail.com>
2018-07-16 19:53:00 -0300

    added media URL provider for ContAR Platform Channels

---
+++ src/main/java/com/colgado/pojo/ContARDataPojo.java
@@ -0,0 +1,15 @@
+package com.colgado.pojo;
+
+public class ContARDataPojo {
+
+	private String hls;
+
+	public String getHls() {
+		return hls;
+	}
+
+	public void setHls(String hls) {
+		this.hls = hls;
+	}
+	
+}
--- src/main/java/com/colgado/service/MediaURLProvider.java
+++ src/main/java/com/colgado/service/MediaURLProvider.java
@@ -1,9 +1,11 @@
 package com.colgado.service;
 
+import static com.colgado.utils.Constants.*;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import com.colgado.pojo.ContARPojo;
 import com.colgado.pojo.TelefePojo;
 
 @Component
@@ -19,10 +21,33 @@ public class MediaURLProvider {
 
 	public String getTelefeURL() {
 		LOGGER.info("Looking for Telefe transmission URL");
-		TelefePojo result = (TelefePojo) getMediaURL("http://m3u8.futbollibre.online/hls/telefe2.php",TelefePojo.class.getName());
+		TelefePojo result = (TelefePojo) getMediaURL(SOURCE_TELEFE,TelefePojo.class.getName());
 		return result.getTelefe();
 	}
 	
+	public String getEncuentroURL() {
+		LOGGER.info("Looking for Encuentro transmission URL");
+		ContARPojo result = (ContARPojo) getMediaURL(SOURCE_ENCUENTRO,ContARPojo.class.getName());
+		return result.getData().getHls();
+	}
+
+	public String getDeporTVURL() {
+		LOGGER.info("Looking for DeporTV transmission URL");
+		ContARPojo result = (ContARPojo) getMediaURL(SOURCE_DEPORTV,ContARPojo.class.getName());
+		return result.getData().getHls();
+	}
+
+	public String getPakaPakaURL() {
+		LOGGER.info("Looking for Paka Paka transmission URL");
+		ContARPojo result = (ContARPojo) getMediaURL(SOURCE_PAKAPAKA,ContARPojo.class.getName());
+		return result.getData().getHls();
+	}
+	
+	public String getTecTVURL() {
+		LOGGER.info("Looking for TEC TV transmission URL");
+		ContARPojo result = (ContARPojo) getMediaURL(SOURCE_TECTV,ContARPojo.class.getName());
+		return result.getData().getHls();
+	}
 	
 	private Class<?> getClassName(String clazz) {
 		Class<?> className = null;
