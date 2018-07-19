f407bc466dbce5d8124145a18af0a661201eea35
Gonzalo <gonzalocasal@gmail.com>
2018-07-19 20:38:13 -0300

    Update media

--- src/main/java/com/colgado/controller/TVController.java
+++ src/main/java/com/colgado/controller/TVController.java
@@ -232,6 +232,8 @@ public class TVController implements ColgadoController{
 		String schedule = "http://www.directv.com.ar/guia/ChannelDetail.aspx?id=719";
 		addCommonAttributes(model);
 		addMediaAttributes(model,title,template,schedule);
+		String id = youtube.getId(CHANNEL_A24);
+		model.addAttribute("id", id);
 		return DEFAULT_VIEW;
 	}
 	
