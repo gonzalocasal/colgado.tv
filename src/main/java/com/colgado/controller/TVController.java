c01527e45b71fc194b1c88ca5cae826589f209fa
Gonzalo <gonzalocasal@gmail.com>
2018-10-06 11:12:35 -0300

    update media

--- src/main/java/com/colgado/controller/TVController.java
+++ src/main/java/com/colgado/controller/TVController.java
@@ -37,7 +37,9 @@ public class TVController implements ColgadoController{
 		String title = "Telefe";
 		String template = "telefe";
 		String schedule = "https://www.directv.com.ar/guia/ChannelDetail.aspx?id=123";
-		addCommonAttributes(model);
+        String url = urlProvider.getTelefeURL();
+        model.addAttribute("url", url);
+        addCommonAttributes(model);
 		addMediaAttributes(model,title,template,schedule);
 		return getDefaultView();
 	}
